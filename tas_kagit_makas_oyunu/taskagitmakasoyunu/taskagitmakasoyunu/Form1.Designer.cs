b762139ac981c94841b7370e4c272907b260577d
Mustafa Koca <54147948+mustafakoca99@users.noreply.github.com>
2020-12-22 17:44:39 +0300

    StonePaperScissorsGameWFA
    simple rock paper scissors game. the computer makes a random selection. the user chooses and whoever wins gives +1 to the winning side.

---
+++ tas_kagit_makas_oyunu/taskagitmakasoyunu/taskagitmakasoyunu/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace taskagitmakasoyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ptas = new System.Windows.Forms.PictureBox();
+            this.pkagit = new System.Windows.Forms.PictureBox();
+            this.pmakas = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pkullanici = new System.Windows.Forms.PictureBox();
+            this.ppc = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelkullaniciskor = new System.Windows.Forms.Label();
+            this.labelbilgisayarskor = new System.Windows.Forms.Label();
+            this.labelsonuc = new System.Windows.Forms.Label();
+            this.labelsecilenkullanici = new System.Windows.Forms.Label();
+            this.labelsecilenpc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ptas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pkagit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pmakas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pkullanici)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ppc)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ptas
+            // 
+            this.ptas.Image = global::taskagitmakasoyunu.Properties.Resources.tas;
+            this.ptas.Location = new System.Drawing.Point(12, 12);
+            this.ptas.Name = "ptas";
+            this.ptas.Size = new System.Drawing.Size(103, 76);
+            this.ptas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptas.TabIndex = 0;
+            this.ptas.TabStop = false;
+            this.ptas.Tag = "0";
+            this.ptas.Click += new System.EventHandler(this.Ptas_Click_1);
+            // 
+            // pkagit
+            // 
+            this.pkagit.Image = global::taskagitmakasoyunu.Properties.Resources.kagit;
+            this.pkagit.Location = new System.Drawing.Point(12, 94);
+            this.pkagit.Name = "pkagit";
+            this.pkagit.Size = new System.Drawing.Size(103, 76);
+            this.pkagit.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pkagit.TabIndex = 1;
+            this.pkagit.TabStop = false;
+            this.pkagit.Tag = "1";
+            this.pkagit.Click += new System.EventHandler(this.Ptas_Click_1);
+            // 
+            // pmakas
+            // 
+            this.pmakas.Image = global::taskagitmakasoyunu.Properties.Resources.makas;
+            this.pmakas.Location = new System.Drawing.Point(12, 176);
+            this.pmakas.Name = "pmakas";
+            this.pmakas.Size = new System.Drawing.Size(103, 76);
+            this.pmakas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pmakas.TabIndex = 2;
+            this.pmakas.TabStop = false;
+            this.pmakas.Tag = "2";
+            this.pmakas.Click += new System.EventHandler(this.Ptas_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::taskagitmakasoyunu.Properties.Resources.makas;
+            this.pictureBox1.Location = new System.Drawing.Point(435, 176);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(103, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::taskagitmakasoyunu.Properties.Resources.kagit;
+            this.pictureBox2.Location = new System.Drawing.Point(435, 94);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(103, 76);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::taskagitmakasoyunu.Properties.Resources.tas;
+            this.pictureBox3.Location = new System.Drawing.Point(435, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(103, 76);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "";
+            // 
+            // pkullanici
+            // 
+            this.pkullanici.Location = new System.Drawing.Point(134, 94);
+            this.pkullanici.Name = "pkullanici";
+            this.pkullanici.Size = new System.Drawing.Size(100, 76);
+            this.pkullanici.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pkullanici.TabIndex = 6;
+            this.pkullanici.TabStop = false;
+            // 
+            // ppc
+            // 
+            this.ppc.Location = new System.Drawing.Point(318, 94);
+            this.ppc.Name = "ppc";
+            this.ppc.Size = new System.Drawing.Size(100, 76);
+            this.ppc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ppc.TabIndex = 7;
+            this.ppc.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(268, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "VS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "KULLANICI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(327, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "BİLGİSAYAR";
+            // 
+            // labelkullaniciskor
+            // 
+            this.labelkullaniciskor.AutoSize = true;
+            this.labelkullaniciskor.Location = new System.Drawing.Point(168, 234);
+            this.labelkullaniciskor.Name = "labelkullaniciskor";
+            this.labelkullaniciskor.Size = new System.Drawing.Size(13, 13);
+            this.labelkullaniciskor.TabIndex = 11;
+            this.labelkullaniciskor.Text = "0";
+            // 
+            // labelbilgisayarskor
+            // 
+            this.labelbilgisayarskor.AutoSize = true;
+            this.labelbilgisayarskor.Location = new System.Drawing.Point(353, 234);
+            this.labelbilgisayarskor.Name = "labelbilgisayarskor";
+            this.labelbilgisayarskor.Size = new System.Drawing.Size(13, 13);
+            this.labelbilgisayarskor.TabIndex = 12;
+            this.labelbilgisayarskor.Text = "0";
+            // 
+            // labelsonuc
+            // 
+            this.labelsonuc.AutoSize = true;
+            this.labelsonuc.Location = new System.Drawing.Point(254, 46);
+            this.labelsonuc.Name = "labelsonuc";
+            this.labelsonuc.Size = new System.Drawing.Size(0, 13);
+            this.labelsonuc.TabIndex = 13;
+            // 
+            // labelsecilenkullanici
+            // 
+            this.labelsecilenkullanici.AutoSize = true;
+            this.labelsecilenkullanici.Location = new System.Drawing.Point(168, 176);
+            this.labelsecilenkullanici.Name = "labelsecilenkullanici";
+            this.labelsecilenkullanici.Size = new System.Drawing.Size(0, 13);
+            this.labelsecilenkullanici.TabIndex = 14;
+            // 
+            // labelsecilenpc
+            // 
+            this.labelsecilenpc.AutoSize = true;
+            this.labelsecilenpc.Location = new System.Drawing.Point(353, 176);
+            this.labelsecilenpc.Name = "labelsecilenpc";
+            this.labelsecilenpc.Size = new System.Drawing.Size(0, 13);
+            this.labelsecilenpc.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 263);
+            this.Controls.Add(this.labelsecilenpc);
+            this.Controls.Add(this.labelsecilenkullanici);
+            this.Controls.Add(this.labelsonuc);
+            this.Controls.Add(this.labelbilgisayarskor);
+            this.Controls.Add(this.labelkullaniciskor);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ppc);
+            this.Controls.Add(this.pkullanici);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pmakas);
+            this.Controls.Add(this.pkagit);
+            this.Controls.Add(this.ptas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ptas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pkagit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pmakas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pkullanici)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ppc)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ptas;
+        private System.Windows.Forms.PictureBox pkagit;
+        private System.Windows.Forms.PictureBox pmakas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pkullanici;
+        private System.Windows.Forms.PictureBox ppc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelkullaniciskor;
+        private System.Windows.Forms.Label labelbilgisayarskor;
+        private System.Windows.Forms.Label labelsonuc;
+        private System.Windows.Forms.Label labelsecilenkullanici;
+        private System.Windows.Forms.Label labelsecilenpc;
+    }
+}
+
---
+++ tas_kagit_makas_oyunu/taskagitmakasoyunu/taskagitmakasoyunu/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taskagitmakasoyunu
+{
+    public partial class Form1 : Form
+    {
+        List<hareket> hl = new List<hareket>();
+        public Form1()
+        {
+            InitializeComponent();
+            //tas için
+            hareket tas = new hareket();
+            tas.metin = "tas";
+            tas.numara = 0;
+            tas.resim = taskagitmakasoyunu.Properties.Resources.tas;
+            //kagit için
+            hareket kagit = new hareket();
+            kagit.metin = "kağıt";
+            kagit.numara = 1;
+            kagit.resim = taskagitmakasoyunu.Properties.Resources.kagit;
+            //makas için
+            hareket makas = new hareket();
+            makas.metin = "makas";
+            makas.numara = 2;
+            makas.resim = taskagitmakasoyunu.Properties.Resources.makas;
+            //list içine ekle
+            hl.Add(tas);
+            hl.Add(kagit);
+            hl.Add(makas);
+        }
+        public class hareket
+        {
+            public int numara { get; set; }
+            public string metin { get; set; }
+            public Bitmap resim { get; set; }
+        }
+        private void Ptas_Click_1(object sender, EventArgs e)
+        {
+            int tag = Convert.ToInt32((sender as PictureBox).Tag);
+            var secilen = hl[tag];
+            //kullanıcı hareketini ortadaki picturebox a al!!!
+            pkullanici.Image = secilen.resim;
+            labelsecilenkullanici.Text = secilen.metin;
+            //pc hareketini ortadaki pictureboxa al!!!
+            Random rnd = new Random();
+            int csecilenid = rnd.Next(0, 3);
+            var csecilen = hl[csecilenid];
+            ppc.Image = csecilen.resim;
+            labelsecilenpc.Text = csecilen.metin;
+            //kim kazandı berabere mi kaldı?
+            if (secilen.numara==csecilen.numara)
+            {
+                labelsonuc.Text = "BERABERE!";
+                labelsonuc.ForeColor = Color.Black;
+            }
+            else if (secilen.numara==0&&csecilen.numara==3||
+                secilen.numara==1&&csecilen.numara==0||
+                secilen.numara==2&&csecilen.numara==1
+                )
+            {
+                labelsonuc.Text = "KULLANICI KAZANDI!";
+                labelsonuc.ForeColor = Color.Green;
+                labelkullaniciskor.Text = (int.Parse(labelkullaniciskor.Text)+1).ToString();
+                if (labelkullaniciskor.Text=="5")
+                {
+                    MessageBox.Show("KULLANICI KAZANDI!");
+                    Application.Restart();
+                }
+            }
+            else
+            {
+                labelsonuc.Text = "BİLGİSAYAR KAZANDI!";
+                labelsonuc.ForeColor = Color.Red;
+                labelbilgisayarskor.Text = (int.Parse(labelbilgisayarskor.Text)+1).ToString();
+                if (labelbilgisayarskor.Text=="5")
+                {
+                    MessageBox.Show("BİLGİSAYAR KAZANDI!");
+                    Application.Restart();
+                }
+            }
+        }
+    }
+}
